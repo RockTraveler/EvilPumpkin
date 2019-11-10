860fc07e1fc0343a8a5c3306a9deab64e1ff453d
Ripper Jin <2654785@qq.com>
2019-11-10 21:52:36 +0800

    Update index controller

--- src/main/java/com/evil/pumpkin/controller/IndexController.java
+++ src/main/java/com/evil/pumpkin/controller/IndexController.java
@@ -4,8 +4,11 @@ package com.evil.pumpkin.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.time.LocalDateTime;
 import java.util.Date;
 import java.util.Map;
@@ -19,8 +22,14 @@ public class IndexController {
 
     @RequestMapping("/index")
     public String index() {
+        return "index";
+    }
 
 
+    @RequestMapping(value = "update", method = RequestMethod.POST)
+    public String update(HttpServletRequest request, HttpServletResponse response) {
+
+        System.out.println(request.getParameter("serverChan_url"));
 
         return "index";
     }
